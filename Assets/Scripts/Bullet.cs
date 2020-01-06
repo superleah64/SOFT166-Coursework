31c8e6e8b9fc9416d81773d0135cea012d66f734
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2020-01-06 15:55:58 +0000

    Collision border applied!
    Character no longer goes through walls

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed = 100.0f;
